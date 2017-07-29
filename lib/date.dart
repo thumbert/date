1a4c99fa26fa6afd3e791a5461f6c5404ef87afd
thumberts <tony.humberts@gmail.com>
2017-07-28 22:23:46 -0400

    and expose the utils

--- lib/date.dart
+++ lib/date.dart
@@ -2,15 +2,12 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 /// The date library.
-
-/**
- * A simple implementation of a Date object backed by an integer.
- */
 library date;
 
 export 'src/date_base.dart';
-export 'src/time_iterable.dart';
-export 'src/month.dart';
-export 'src/myy.dart';
 export 'src/hour.dart';
 export 'src/interval.dart';
+export 'src/month.dart';
+export 'src/myy.dart';
+export 'src/time_iterable.dart';
+export 'src/utils.dart';
