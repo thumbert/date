87ad7796f62a278cedd406d1083754ab20b00c73
thumbert <humbert.tony@yahoo.com>
2017-08-04 15:40:42 -0400

    add tests tp splitIntoHours

--- test/interval_test.dart
+++ test/interval_test.dart
@@ -1,12 +1,18 @@
 
 library test.elec.interval;
 
-
+import 'dart:io';
 import 'package:test/test.dart';
+import 'package:timezone/standalone.dart';
 import 'package:date/src/interval.dart';
 
 main() {
-
+  Map env = Platform.environment;
+  String tzdb = env['HOME'] +
+      '/.pub-cache/hosted/pub.dartlang.org/timezone-0.4.3/lib/data/2015b.tzf';
+  initializeTimeZoneSync(tzdb);
+  Location location = getLocation('US/Eastern');
+  
   test('interval', () {
     Interval i1 = new Interval(new DateTime(2015,1,1), new DateTime(2015,1,2));
     Interval i2 = new Interval(new DateTime(2015,1,1), new DateTime(2015,1,2));
@@ -47,4 +53,28 @@ main() {
     expect(i.toString(), '2015-01-01 00:00:00.000');
   });
 
+  
+  test('splitting into hours across hour boundary, returns two hours', () {
+    TZDateTime start = new TZDateTime(location, 2017,1,1);
+    TZDateTime end = new TZDateTime(location, 2017,1,1,1,15);
+    Interval interval = new Interval(start, end);
+    List res = splitIntoHours(interval);
+    expect(res.length, 2);
+  });
+  test('splitting into hours an interval less than an hour, returns an empty list', () {
+    TZDateTime start = new TZDateTime(location, 2017,1,1,1,10);
+    TZDateTime end = new TZDateTime(location,   2017,1,1,1,15);
+    Interval interval = new Interval(start, end);
+    List res = splitIntoHours(interval);
+    expect(res.length, 0);
+  });
+  test('splitting into hours an exact hour, returns the hour', () {
+    TZDateTime start = new TZDateTime(location, 2017,1,1,0);
+    TZDateTime end = new TZDateTime(location,   2017,1,1,1);
+    Interval interval = new Interval(start, end);
+    List res = splitIntoHours(interval);
+    expect(res.length, 1);
+  });
+  
+  
 }
