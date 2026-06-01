1057821be324f1ebd245396e1afd86615b7c70c6
thumbert <tony.humberts@gmail.com>
2026-06-01 19:51:16 -0400

    2026-06-01

--- lib/src/hour.dart
+++ lib/src/hour.dart
@@ -9,16 +9,6 @@ import 'package:date/src/interval.dart';
 class Hour extends Interval implements TimeOrdering<Hour>, Additive<Hour> {
   static final Duration _H1 = Duration(hours: 1);
 
-  /// Truncate [dt] to the start of its clock hour using epoch arithmetic,
-  /// so that the correct DST occurrence is preserved.
-  static TZDateTime _truncateToHour(TZDateTime dt) {
-    final ms = dt.millisecondsSinceEpoch;
-    return TZDateTime.fromMillisecondsSinceEpoch(
-      dt.location,
-      ms - ms % Duration.millisecondsPerHour,
-    );
-  }
-
   /// Create an hour containing a given [TZDateTime].
   /// At Fall DST transition this correctly returns the hour that actually
   /// contains [dt], including the repeated 01:xx EST hour.
@@ -72,4 +62,15 @@ class Hour extends Interval implements TimeOrdering<Hour>, Additive<Hour> {
 
   /// return this hour as an Interval
   Interval toInterval() => Interval(start, end);
+
+  /// Truncate [dt] to the start of its clock hour using epoch arithmetic,
+  /// so that the correct DST occurrence is preserved.
+  static TZDateTime _truncateToHour(TZDateTime dt) {
+    final ms = dt.millisecondsSinceEpoch;
+    return TZDateTime.fromMillisecondsSinceEpoch(
+      dt.location,
+      ms - ms % Duration.millisecondsPerHour,
+    );
+  }
+
 }
